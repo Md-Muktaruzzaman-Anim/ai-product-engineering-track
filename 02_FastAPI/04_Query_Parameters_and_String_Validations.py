5793eda711b863ef4f317ff6ff597e56174df67a
Md Muktaruzzaman <mdmuktaruzzamananim@gmail.com>
2026-06-11 11:36:55 +0600

    Add FastAPI project files

---
+++ 02_FastAPI/04_Query_Parameters_and_String_Validations.py
@@ -0,0 +1,24 @@
+from typing import Annotated
+
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+
+@app.get("/items/")
+async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.get("/items/")
+async def read_items(
+    q: Annotated[
+        str | None, Query(min_length=3, max_length=50, pattern="^fixedquery$")
+    ] = None,
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
